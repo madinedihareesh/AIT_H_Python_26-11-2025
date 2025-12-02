762cbca5d99b5a464c8217e9254b733adb94e183
Hareesh <hareesh.madinedi@gmail.com>
2025-12-02 11:06:34 +0530

    Day4

---
+++ Day3_Datatypes/ex.py
@@ -0,0 +1,56 @@
+#Data type in python
+#variable
+# charters=value
+# declaration=initalization
+'''
+Rules:
+1.variable should contain alphanumaric values,_
+2.it has be staring with either alphabet or _
+3.it cannot be staring number or numarics
+4.it is casesencitive
+5.Keywords should not be descried as a variable
+'''
+variable=1
+VARIABLE=2
+Variable=3
+_1variable=5
+_variable=4
+print(variable,VARIABLE,Variable,_variable,_1variable)
+a=20
+print(id(a))
+
+
+a=a+10
+print(a)
+print(id(a))
+'''
+types in datatypes:
+1.primitive:
+a.int
+b.float
+c.bool
+d.complex 10+i2 i=squrt(-1)
+e.string
+'''
+i=123245345
+print(type(i))
+f=1259e-2
+print(f)
+print(type(f))
+b1=True
+b2=False
+print(type(b1),type(b2))
+c=10+2j
+print(type(c))
+s='Mansoor'
+S="Karthik"
+s1='''HI,
+Hello world
+How are you doing?
+i am doing grate
+curruntly persuing python'''
+print(s,S)
+print(s1)
+
+
+     
---
+++ Day4_Datatypes_oparators/ex.py
@@ -0,0 +1,146 @@
+'''
+0 to 9
+0 to 7
+0 to 15
+int
+float
+bool
+complex
+string
+'''
+'''
+Base conversions
+print(oct(10))
+print(hex(10))
+print(bin(10))
+
+print(int(0b1010))
+print(oct(0b1010))
+
+a=10.00
+print(type(a))
+'''
+
+'''
+Type conversions:
+
+a=10
+print(type(a))
+print(float(a))
+print(complex(a))
+print(bool(-1))
+print(type(str(a)))
+
+b=12.59
+print(int(b))
+print(bool(b))
+print(complex(b))
+print(str(b))
+
+c=True
+print(int(c))
+print(complex(c))
+print(float(c))
+print(str(c))
+
+d=12+9j
+# print(int(d))
+# print(float(d))
+print(bool(d))
+print(str(d))
+
+s='Python'
+print(int('10'))
+print(float('10'))
+print(complex('10'))
+print(bool(s))
+'''
+
+'''
+Non-premitive datatypes:
+sequencial:
+1.List [] index
+2.tuple
+3.arrays[]
+4.bytearrays[]
+
+non-seq:
+1. sets {} 
+2.dist {} 
+'''
+
+'''
+Oparators:
+1.Arthamatic 
+a.+
+b.-
+c.*
+d.%
+e.**
+f./
+g.// floor  division
+'''
+print(10+20)
+print(20-10)
+print(10%2)
+print(10**2)
+print(10/2)
+print(10//2)
+
+'''
+2.Relational or Comparission 
+a.==
+b.!=
+c.>
+d.<
+e.>=
+f.<=
+'''
+'''
+3.Assigment oparators:
+a.=
+b.+=
+c.-=
+d.*=
+e./=
+f.**=
+g.//=
+a=10
+a+=1
+print(a)
+'''
+'''
+Logical oparators:
+a. AND
+b.OR
+c.Not
+'''
+
+'''
+Bitiwse oparators:
+& and
+backtick or
+~ not
+>> right shift
+<< left shift
+'''
+'''
+membership oparators:
+
+in, not in
+
+'''
+'''
+identity oparators:
+is ,is not
+a=10
+b=20
+print(id(a))
+print(id(b))
+print(a is b)
+'''
+
+'''
+terinary oparator:
+'''
+
