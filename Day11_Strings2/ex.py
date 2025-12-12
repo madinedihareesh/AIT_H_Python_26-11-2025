50c53974b81bae2d6128c2132971cd050ba7cb3b
Hareesh <hareesh.madinedi@gmail.com>
2025-12-12 16:10:22 +0530

    Day11_Python_Strings2

---
+++ Day11_Strings2/ex.py
@@ -0,0 +1,48 @@
+# s1='AchieversIT'
+# s2='a-b-c-d-e'
+# s3='Html,is,easy,to,learn'
+# print(s3.replace('Html','Python'))
+# s1='Python'
+# print(s3.split(','))
+
+# s1='Python is easy to lear'
+
+# print(s1.split(' ',2))
+
+# print(s1.rsplit(' ',2))
+
+# s2='''
+#    Hi,
+#    How are you
+#    are you doing grate
+#    i am learing python
+# '''
+
+# s3='Hi\nHow are you\nare you doing fine\ni am doing grate'
+
+# print(s3.splitlines())
+
+
+# s1='python is easy'
+# print(s1.startswith('python'))
+
+# print(s1.endswith('easy'))
+
+
+# print(s1.removeprefix('p'))
+# print(s1.removesuffix('y'))
+# email='sample@gmail.com'
+# print(email.partition('@'))
+
+# name='AchieversIT is a training'
+# print(name.upper())
+# print(name.lower())
+# print(name.title())
+# print(name.casefold())
+# print(name.capitalize())
+
+# print(name.swapcase())
+
+print(isinstance(True,bool))
+
+
