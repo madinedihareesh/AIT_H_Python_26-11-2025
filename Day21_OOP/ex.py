ad7e93a3d93e7b740203890a80ab5a21ae637793
Hareesh <hareesh.madinedi@gmail.com>
2026-01-02 18:20:57 +0530

    Day21_OOP

---
+++ Day21_OOP/ex.py
@@ -0,0 +1,37 @@
+'''
+class nameofclass(parameters):
+    def __init__():
+        varibles
+    def method(parametsrs):
+        block of code
+'''
+'''
+class Demo():
+    
+    def greet(self):
+        print('Hello world')
+
+# d=Demo()  
+# d1=Demo()
+
+# d.greet()
+# d1.greet()
+Demo().greet()
+'''
+
+class Demo():
+    some='Data'
+    def __init__(self):
+        pass
+    def Displayname(self,name):
+        print(name)    
+    def Displayage(self,age):
+        print(age)
+
+I1=Demo()
+I1.Displayname('AchieversIT') 
+I1.Displayage(11)
+print(I1.some)  
+print(Demo.some)         
+
+        
